2cd297474b6603067584f83c5b16fea1bb6641f5
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2025-03-02 16:40:14 -0300

    feat: ensure cart quantity can be decreased

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -13,6 +13,7 @@ export interface ICartContext {
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -20,6 +21,7 @@ export const CartContext = createContext<ICartContext>({
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
+  decreaseProductQuantity: () => {},
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -31,23 +33,40 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
 
   const addProduct = (product: CartProduct) => {
     const productIsAlreadyOnTheCart = products.some(
-        (prevProduct) => prevProduct.id === product.id,
-      );
-      if (!productIsAlreadyOnTheCart) {
-        return setProducts((prev) => [...prev, product]);
-      }
-      setProducts((prevProducts) => {
-        return prevProducts.map((prevProduct) => {
-          if (prevProduct.id === product.id) {
-            return {
-              ...prevProduct,
-              quantity: prevProduct.quantity + product.quantity,
-            };
-          }
-          return prevProduct;
-        });
+      (prevProduct) => prevProduct.id === product.id,
+    );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
       });
-    };
+    });
+  };
+
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
+        }
+        if (prevProduct.quantity === 1) {
+          return prevProduct;
+        }
+        return {
+          ...prevProduct,
+          quantity: prevProduct.quantity - 1,
+        };
+      });
+    });
+  };
 
   return (
     <CartContext.Provider
@@ -56,6 +75,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         products,
         toggleCart,
         addProduct,
+        decreaseProductQuantity,
       }}
     >
       {children}
